ee3e5bed48e7adf5a08f132a437587e1273caebb
liimonx <limonkhan669@gmail.com>
2018-11-03 20:48:19 +0600

    Updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 var options = {
-  valueNames: [ 'shape', 'carat', 'color', 'clarity', 'cut', 'polish', 'symmetry', 'lab', 'price' ]
+  valueNames: ['shape', 'carat', 'color', 'clarity', 'cut', 'polish', 'symmetry', 'lab', 'price'],
+  page: 17,
+  pagination: {
+    innerWindow: 1,
+    left: 0,
+    right: 0,
+    paginationClass: "pagination",
+  }
 };
 
-var userList = new List('results', options);
+var resultList = new List('diamondsearch', options);
+
+
+$('.jPaginateNext').on('click', function () {
+  var list = $('.pagination').find('li');
+  $.each(list, function (position, element) {
+    if ($(element).is('.active')) {
+      $(list[position + 1]).trigger('click');
+    }
+  })
+});
+
+
+$('.jPaginatePrev').on('click', function () {
+  var list = $('.pagination').find('li');
+  $.each(list, function (position, element) {
+    if ($(element).is('.active')) {
+      $(list[position - 1]).trigger('click');
+    }
+  })
+});
